f043a0056ede5412a7cdfd1ca5f50bada74596a4
aviv deri <aviv322deri@gmail.com>
2026-09-06 11:48:33 +0300

    refactor: migrate to expo root component and configure android build properties for NFC support

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import { AppRegistry, Platform } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './App';
 
-function Root() {
-  const Component = App?.default || App;
-  return <Component />;
-}
+registerRootComponent(App);
 
-AppRegistry.registerComponent('main', () => Root);
-
-if (Platform.OS === 'web' && typeof window !== 'undefined') {
-  const rootTag = document.getElementById('root');
-  if (rootTag) {
-    AppRegistry.runApplication('main', { rootTag });
-  }
-}
